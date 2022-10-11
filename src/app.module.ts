2d70a3854bf71a1c994b4cbe85cb0da3a69824cf
eddiEkpenyong <edidiong.ekpenyong.19@gmail.com>
2022-10-11 15:39:49 +0100

    db_setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, AuthModule, ConfigModule.forRoot(), TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: process.env.MYSQL_HOST,
+    port: 3306,
+    username: process.env.MYSQL_USERNAME,
+    password: '',
+    database: process.env.MYSQL_DATABASE,
+    entities: [],
+    synchronize: true
+  })],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
